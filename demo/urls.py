f2a52e9a5d281a73a43e6f6c4a06370be5764777
mdp89 <marcodaniele.pellizzari@gmail.com>
2017-03-05 17:05:42 +0100

    Aggiunti report punteggio

--- demo/urls.py
+++ demo/urls.py
@@ -17,6 +17,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.home, name='home'),
+    url(r'^dettagli/(\d{1,3})/',views.dettagli_gara),
     url(r'^login/$','django.contrib.auth.views.login',name='login'),
     url(r'^logout/$','django.contrib.auth.views.logout',name='logout'),
     url(r'^logout-then-login/$','django.contrib.auth.views.logout_then_login',name='logout_then_login'),
--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'demo/home.html',{'titolo':'Home Page'})
+    gare=Gara.objects.all()
+    return render(request, 'demo/home.html',{'titolo':'Home Page','gare':gare})
+
+
+def calcola_punti(idgara):
+    gara=Gara.objects.filter(id=idgara).get()
+    num_domande=gara.num_domande
+    domande=gara.domanda_set
+    squadre=gara.squadre.all()
+    scoreboard=[]
+    for i in squadre:
+        punteggi={}
+        punteggi['squadra']=i.nome
+        punteggi['punti']=[0]*num_domande
+        for j in range(1,num_domande+1):
+            somma=0
+            dom=domande.filter(num=j)
+            risp=i.risposta_set.filter(domanda=dom).all()
+            for k in risp:
+                if k.risposta==dom.risp:
+                    somma=somma+100
+                else:
+                    somma=somma-10
+            punteggi['punti'][j-1]=somma
+        punteggi['totale']=sum(punteggi['punti'])
+        scoreboard.append(punteggi)
+    return scoreboard
+
+
+def dettagli_gara(request,idgara):
+    idgara=int(idgara)
+    gara=Gara.objects.filter(id=idgara).get()
+    num_domande=gara.num_domande
+    scores=calcola_punti(idgara)
+    return render(request, 'demo/dettagli_gara.html',
+            {'scores':scores,'num':range(1,num_domande+1)})
+
+
+
+
